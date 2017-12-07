e23b85db1d872408349daddaf777cc9beee8d253
medyt <madalin.andone@gmail.com>
2017-12-07 15:58:01 +0200

    config update

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    $servername = "servername";
+    $username = "unername for connect to database";
+    $password = "password for connection database";
+    $name = 'the name of database';
+    $loginusername = 'login username';
+    $loginpassword = 'login password';
+?>
